b529ab71925a91c82d93488c7ad0cfbce013be57
prthm20 <103777268+prthm20@users.noreply.github.com>
2023-08-26 07:39:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -157,6 +157,10 @@ app.post("/", function (req, res) {
         res.render('about')
         
       })
+let port = process.env.PORT;
+if(port==null ||port=""){
+  port=3000
+}
       
       app.listen(3000, function () {
         console.log("Server started on port 3000");
